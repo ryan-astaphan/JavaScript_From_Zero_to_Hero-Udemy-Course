9b5ebe8ca156094f6b741f99cabd2ee1e23e66c8
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-09 16:07:06 +0700

    LECTURE: Data Types

--- practice/Lesson 1/assignments.js
+++ practice/Lesson 1/assignments.js
@@ -1,3 +1,13 @@
+// LECTURE: Values and Variables
 let country = 'Thailand'
 let continent = 'Asia'
 let population = 70
+
+// LECTURE: Data Types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof language);
+console.log(typeof country);
--- practice/Lesson 1/script.js
+++ practice/Lesson 1/script.js
@@ -1,8 +1,34 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
 let firstName = 'Ryan'
-console.log(firstName);
+// console.log(firstName);
 
-let jsNamingConventions = "In this video the instructor went over \n
-camelCase and other rules and conventions"
+// let jsNamingConventions = "In this video the instructor went over \n
+// camelCase and other rules and conventions";
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// viewing data types with 'typeof'
+// console.log(typeof true);
+// console.log(typeof firstName);
+// console.log(typeof 23);
+// console.log(typeof 'true');
+console.log(typeof javascriptIsFun);
+
+// when RE-ASSIGNING a VALUE we do not use "let"
+javascriptIsFun = 'Yes it is'
+console.log(typeof javascriptIsFun);
+
+// data type 'undefined'
+let year;
+console.log(year);
+console.log(typeof year);
+
+// re-assigning once agian
+year = 1985;
+console.log(typeof year);
+
+// NULL - the typeof bug that never got fixed for legacy purposes
+console.log(typeof null);
