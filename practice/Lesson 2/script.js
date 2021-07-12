a5efab50abeac7b86389b10927cbb6ba5d60e202
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-12 15:53:25 +0700

    Learned a bit about STRICT MODE

---
+++ practice/Lesson 2/script.js
@@ -0,0 +1,10 @@
+'use strict'; // This code turns on strict mode. 
+// For strict mode to work, it has to be the first line of code in the file.
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; // strict mode will call a Reference error
+if (hasDriversLicense) console.log("I can drive.")
+
+const interface = 'Audio'; // strict mode will raise a SyntaxError
