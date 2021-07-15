c8bd5bba729b56e5b8e39ec28e348b07e527b6e8
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-15 10:28:36 +0700

    Learned what the DOM is and what DOM Manipulation is

--- practice/Lesson 5/script.js
+++ practice/Lesson 5/script.js
@@ -1,3 +1,17 @@
 'use strict';
 
+// BEGINNING THE GUESS-MY-NUMBER PROJECT
 console.log(document.querySelector('.message').textContent);
+
+// LECTURE: What's the DOM and DOM Manipulation?
+// DOM = Document Object Model:
+// DOM: a structured representation of an HTML documents.
+// The DOM allows JavaScript to access HTML elements and
+// styles in order to manipulate them.
+
+// "document" is a special object that is the entry-point
+// to the DOM. That's why we use document.querySelector()
+// to access an element.
+
+// The DOM is not JavaScript
+// It's actually part of the WEB APIs that web browsers use.
