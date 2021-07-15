bffc8cb9f45f93447c3df52b6fd096c8c23622ac
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-15 10:38:41 +0700

    Practiced selecting and manipulating elements with the DOM

--- practice/Lesson 5/script.js
+++ practice/Lesson 5/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 // BEGINNING THE GUESS-MY-NUMBER PROJECT
-console.log(document.querySelector('.message').textContent);
-
+// console.log(document.querySelector('.message').textContent);
+// ------------
+// ------------
+// ------------
 // LECTURE: What's the DOM and DOM Manipulation?
 // DOM = Document Object Model:
 // DOM: a structured representation of an HTML documents.
@@ -12,6 +14,20 @@ console.log(document.querySelector('.message').textContent);
 // "document" is a special object that is the entry-point
 // to the DOM. That's why we use document.querySelector()
 // to access an element.
-
+//
 // The DOM is not JavaScript
 // It's actually part of the WEB APIs that web browsers use.
+// ------------
+// ------------
+// ------------
+// LECTURE: Selecting and Manipulating Elements
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 \
+Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
