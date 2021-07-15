1592f35a7bcc66d239ca6988c847d2fb57ac482c
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-15 11:38:46 +0700

    LECTURE: Handling Click Events

--- practice/Lesson 5/script.js
+++ practice/Lesson 5/script.js
@@ -21,13 +21,25 @@
 // ------------
 // ------------
 // LECTURE: Selecting and Manipulating Elements
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 \
-Correct Number!';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 \
+// Correct Number!';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+// ------------
+// ------------
+// ------------
+// LECTURE: Handling Click Events
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ Enter a number!';
+  }
+});
