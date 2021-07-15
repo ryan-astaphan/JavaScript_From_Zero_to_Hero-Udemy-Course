577d1cd6be926e93a953ea3c337152a9c3828829
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-15 14:44:21 +0700

    LECTURE: Refactoring Our Code - The DRY Principle

--- practice/Lesson 5/script.js
+++ practice/Lesson 5/script.js
@@ -51,17 +51,27 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   //   When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ Enter a number!';
+    displayMessage('⛔ Enter a number!');
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 CORRECT!';
-    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('🎉 CORRECT!');
+    displayNumber(secretNumber);
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     if (score > highScore) {
@@ -70,46 +80,34 @@ document.querySelector('.check').addEventListener('click', function () {
         '.label-highscore'
       ).textContent = `🥇 Highscore: ${highScore}`;
     }
-  }
-  // When guess is too high
-  else if (guess > secretNumber) {
+
+    // When guess is incorrect
+  } else if (guess !== secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent =
-        '📈 You guessed too high!';
+        guess > secretNumber
+          ? '📈 You guessed too high!'
+          : '📉 You guessed too low!';
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
+      displayMessage('💥 You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
     // When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent =
-        '📉 You guessed too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
   }
 });
 // ------------
 // ------------
 // ------------
-// CODING CHALLENGE #1
+// CODING CHALLENGE #1 - Implementing the Play Again button
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.number').textContent = '?';
+  displayScore(score);
+  displayMessage('Start guessing...');
+  displayNumber('?');
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-// ------------
-// ------------
-// ------------
-// LECTURE: High Score Implemenation
