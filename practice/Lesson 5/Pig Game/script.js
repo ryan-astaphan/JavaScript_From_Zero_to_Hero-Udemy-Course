45b126ed4b162086aeacd427a8f0b3e3069be8d1
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-16 10:23:28 +0700

    New game button functionality added. Up until now I've done all the coding by myself, but now I'm going to go back and follow the video tutorial

--- practice/Lesson 5/Pig Game/script.js
+++ practice/Lesson 5/Pig Game/script.js
@@ -3,17 +3,19 @@
 // Player scores
 let player1Score = 0;
 let player2Score = 0;
-// document.querySelector('#score--0').textContent = 69;
-// document.querySelector('#score--1').textContent = 13;
+document.querySelector('#score--0').textContent = 0;
 
 // Current tallies
 let player1Tally = 0;
 let player2Tally = 0;
-// document.querySelector('#current--1').textContent = 15;
+document.querySelector('#score--1').textContent = 0;
 
 // TURN and ROLL Logic
 let playerOnesTurn = true;
+
+// ROLL DICE BUTTON
 document.querySelector('.btn--roll').addEventListener('click', function () {
+  //
   //
   //
   //   ROLLING THE DICE
@@ -25,29 +27,74 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
   //   PLAYER TURNS - dice roll logic
   //   player 1's turn
   if (playerOnesTurn) {
+    //   if player rolls a 1
     if (diceRoll === 1) {
       player1Tally = 0;
       document.querySelector('#current--0').textContent = player1Tally;
       playerOnesTurn = false;
-    } else {
+      document.querySelector('.player--0').classList.remove('player--active');
+      document.querySelector('.player--1').classList.add('player--active');
+    }
+    // if player rolls any other #
+    else {
       player1Tally += diceRoll;
-      console.log('Player 1 tally: ', player1Tally);
+      console.log('Player 1 rolled a: ', diceRoll);
       document.querySelector('#current--0').textContent = player1Tally;
     }
   }
   //   player 2's turn
   else {
+    //   if player rolls a 1
     if (diceRoll === 1) {
       player2Tally = 0;
-      document.querySelector('#current--1').textContent = player1Tally;
+      document.querySelector('#current--1').textContent = player2Tally;
       playerOnesTurn = true;
-    } else {
+      document.querySelector('.player--0').classList.add('player--active');
+      document.querySelector('.player--1').classList.remove('player--active');
+    }
+    // if player rolls any other #
+    else {
       player2Tally += diceRoll;
-      console.log('Player 2 tally: ', player2Tally);
-      document.querySelector('#current--1').textContent = player1Tally;
+      console.log('Player 2 rolled a: ', diceRoll);
+      document.querySelector('#current--1').textContent = player2Tally;
     }
   }
-
-  // random roll between 1-6 ... add number to current total ... update image of dice
-  // if diceRoll === 1, lose current and switch turns
+});
+//
+//
+// HOLD BUTTON
+document.querySelector('.btn--hold').addEventListener('click', function () {
+  if (playerOnesTurn) {
+    player1Score += player1Tally;
+    document.querySelector('#score--0').textContent = player1Score;
+    player1Tally = 0;
+    document.querySelector('#current--0').textContent = player1Tally;
+    playerOnesTurn = false;
+    document.querySelector('.player--0').classList.remove('player--active');
+    document.querySelector('.player--1').classList.add('player--active');
+  } else {
+    player2Score += player2Tally;
+    document.querySelector('#score--1').textContent = player2Score;
+    player2Tally = 0;
+    document.querySelector('#current--1').textContent = player2Tally;
+    playerOnesTurn = true;
+    document.querySelector('.player--0').classList.add('player--active');
+    document.querySelector('.player--1').classList.remove('player--active');
+  }
+});
+//
+//
+// NEW GAME BUTTON
+document.querySelector('.btn--new').addEventListener('click', function () {
+  player1Score = 0;
+  player2Score = 0;
+  document.querySelector('#score--0').textContent = 0;
+  document.querySelector('#score--1').textContent = 0;
+  player1Tally = 0;
+  player2Tally = 0;
+  document.querySelector('#current--0').textContent = 0;
+  document.querySelector('#current--1').textContent = 0;
+  playerOnesTurn = true;
+  document.querySelector('.player--0').classList.add('player--active');
+  document.querySelector('.player--1').classList.remove('player--active');
 });
