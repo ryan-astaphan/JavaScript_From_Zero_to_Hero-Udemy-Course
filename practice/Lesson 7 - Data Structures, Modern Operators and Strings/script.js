e1a457a79c4e7c28a55e3f878232627d10d033be
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-19 12:36:29 +0700

    LESSON: Working With Strings Part 3

--- practice/Lesson 7 - Data Structures, Modern Operators and Strings/script.js
+++ practice/Lesson 7 - Data Structures, Modern Operators and Strings/script.js
@@ -371,56 +371,100 @@ const checkMiddleSeat = function (seat) {
 // ------------------------------------------------
 // ------------------------------------------------
 
-// LESSON: Working With Strings Part 2
-console.log(airline.toLowerCase());
-console.log(airline.toUpperCase());
+// // LESSON: Working With Strings Part 2
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
 
-// fix capitalization
-const passenger = 'jOnas';
-const passengerLower = passenger.toLowerCase();
-const passengerCorrect =
-  passengerLower[0].toUpperCase() + passengerLower.slice(1);
-console.log(passengerCorrect);
+// // fix capitalization
+// const passenger = 'jOnas';
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect =
+//   passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
 
-// comparing emails
-const email = 'hello@jonas.io';
-const loginEmail = '  Hello@Jonas.Io \n';
-const lowerEmail = loginEmail.toLowerCase();
-const trimmedEmail = lowerEmail.trim();
-// a much simpler way to do the above normalization
-const normalizedEmail = loginEmail.toLowerCase().trim();
-console.log(normalizedEmail);
+// // comparing emails
+// const email = 'hello@jonas.io';
+// const loginEmail = '  Hello@Jonas.Io \n';
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// // a much simpler way to do the above normalization
+// const normalizedEmail = loginEmail.toLowerCase().trim();
+// console.log(normalizedEmail);
 
-// replacing parts of strings
-const priceGB = '£288,97';
-const priceUS = priceGB.replace(',', '.').replace('£', '$');
-console.log(priceUS);
+// // replacing parts of strings
+// const priceGB = '£288,97';
+// const priceUS = priceGB.replace(',', '.').replace('£', '$');
+// console.log(priceUS);
 
-const announcement =
-  'All passengers come to boarding door 23. Boarding door 23.';
-console.log(announcement.replaceAll('door', 'gate'));
+// const announcement =
+//   'All passengers come to boarding door 23. Boarding door 23.';
+// console.log(announcement.replaceAll('door', 'gate'));
 
-// Booleans
-plane = 'Airbus A320neo';
-console.log(plane.includes('A320'));
-console.log(plane.includes('Boeing'));
-console.log(plane.startsWith('Air'));
-console.log(plane.startsWith('A3'));
+// // Booleans
+// plane = 'Airbus A320neo';
+// console.log(plane.includes('A320'));
+// console.log(plane.includes('Boeing'));
+// console.log(plane.startsWith('Air'));
+// console.log(plane.startsWith('A3'));
 
-if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
-  console.log('You are flying on a brand-new airplane!');
-}
+// if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+//   console.log('You are flying on a brand-new airplane!');
+// }
 
-// practice exercise
-const checkBaggage = function (items) {
-  const baggage = items.toLowerCase();
-  if (baggage.includes('knife') || baggage.includes('gun')) {
-    console.log('You are NOT allowed on the airplane');
-  } else {
-    console.log('You are ready and able to board the airplane');
+// // practice exercise
+// const checkBaggage = function (items) {
+//   const baggage = items.toLowerCase();
+//   if (baggage.includes('knife') || baggage.includes('gun')) {
+//     console.log('You are NOT allowed on the airplane');
+//   } else {
+//     console.log('You are ready and able to board the airplane');
+//   }
+// };
+
+// checkBaggage('I have a laptop, some food and a pocket knife.');
+// checkBaggage('Just some books');
+// checkBaggage('Got my hunting rifle, aka a gun, in my bag!');
+
+// ------------------------------------------------
+// ------------------------------------------------
+// ------------------------------------------------
+
+// LESSON: Working With Strings Part 3
+// split method
+console.log('a+very+nice+string'.split('+'));
+console.log('Barry Sanders'.split(' '));
+const [firstName, lastName] = 'Barry Sanders'.split(' ');
+console.log(firstName);
+console.log(lastName);
+
+// join method
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesTitle = [];
+  for (const n of names) {
+    namesTitle.push(n[0].toUpperCase() + n.slice(1));
+    // can also do it like this:
+    // namesTitle.push(n.replace(n[0], n[0].toUpperCase()))
   }
+  console.log(namesTitle);
 };
+capitalizeName('mary ann taylor');
+capitalizeName('dedrick rodgers');
 
-checkBaggage('I have a laptop, some food and a pocket knife.');
-checkBaggage('Just some books');
-checkBaggage('Got my hunting rifle, aka a gun, in my bag!');
+// padding a string
+const message = 'Go to gate 23';
+console.log(message.padStart(25, '+'));
+console.log('Ryan'.padStart(25, '+').padEnd(35, '-'));
+
+const maskCreditCard = function (number) {
+  const str = number + ''; // this will CONVERT # TO a STRING
+  console.log(str.slice(-4).padStart(32, '*'));
+};
+maskCreditCard(4242424242424242);
+
+// repeat method
+const message2 = 'Bad weather... All departures delayed... ';
+console.log(message2.repeat(4));
