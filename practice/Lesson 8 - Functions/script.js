7aa986c4d6851c7534f815ca117f9669d8502caf
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-19 12:45:15 +0700

    Lesson: Default Parameters

---
+++ practice/Lesson 8 - Functions/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// FUNCTIONS
+//
+// LESSON: Default Parameters
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH123');
+createBooking('LH763', 2, 800);
+createBooking('LH763', 3);
