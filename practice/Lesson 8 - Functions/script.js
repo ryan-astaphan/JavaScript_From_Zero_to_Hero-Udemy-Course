159c06f6508c43859395bf527222b652f89d7f3b
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-19 13:56:01 +0700

    LESSON: How Passing Arguments Works - Value vs. Reference

--- practice/Lesson 8 - Functions/script.js
+++ practice/Lesson 8 - Functions/script.js
@@ -21,3 +21,38 @@ const createBooking = function (
 createBooking('LH123');
 createBooking('LH763', 2, 800);
 createBooking('LH763', 3);
+
+//
+//
+//
+
+// LESSON: How Passing Arguments Works - Value vs. Reference
+// ** in JavaScript you can only pass values to functions, not reference.
+// *** a value can reference an object, but it is not considered as passing a reference.
+const flight = 'LH123';
+const ryan = {
+  name: 'Ryan A',
+  passport: 505777933,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 505777933) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, ryan);
+console.log(flight);
+console.log(ryan);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000);
+};
+
+newPassport(ryan);
+checkIn(flight, ryan);
