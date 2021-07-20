eb65af766e86d2cea2154aefeef60be998c4c98c
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-20 16:54:38 +0700

    LESSON: Our First AJAX Call: XMLHttpRequest

--- practice/Lesson 10 - Asynchronous/script.js
+++ practice/Lesson 10 - Asynchronous/script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
-
 ///////////////////////////////////////
 // LESSON: Asynchronous Javascript, AJAX and API
 ///////////////////////////////////////
@@ -42,3 +39,42 @@ const countriesContainer = document.querySelector('.countries');
 //      software in order to allow applications to talk to each other.
 // -    An "Online" API is an application running on a server that
 //      receives requests for data and sends data back as a response.
+
+///////////////////////////////////////
+// LESSON: Our First AJAX Call: XMLHttpRequest
+///////////////////////////////////////
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+  <article class="country">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      data.population / 1000000
+    ).toFixed(1)} million</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>
+`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('Thailand');
+getCountryData('Vietnam');
+getCountryData('Malaysia');
