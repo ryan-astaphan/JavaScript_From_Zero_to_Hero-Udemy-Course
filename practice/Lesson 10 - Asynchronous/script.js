9ca3fa836ee8928052bb11273ff0a3057213d102
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-20 17:26:20 +0700

    LESSON: Promises and the Fetch API

--- practice/Lesson 10 - Asynchronous/script.js
+++ practice/Lesson 10 - Asynchronous/script.js
@@ -83,73 +83,108 @@
 // LESSON: Welcome to Callback Hell
 ///////////////////////////////////////
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
 
-const renderCountry = function (data, className) {
-  const html = `
-  <article class="country ${className}">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      data.population / 1000000
-    ).toFixed(1)} million</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-  </div>
-</article>
-`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
+// const renderCountry = function (data, className) {
+//   const html = `
+//   <article class="country ${className}">
+//   <img class="country__img" src="${data.flag}" />
+//   <div class="country__data">
+//     <h3 class="country__name">${data.name}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(
+//       data.population / 1000000
+//     ).toFixed(1)} million</p>
+//     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//   </div>
+// </article>
+// `;
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+// };
 
-const getCountryAndNeighbor = function (country) {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-  request.send();
+// const getCountryAndNeighbor = function (country) {
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    // Render country #1
-    renderCountry(data);
+//     // Render country #1
+//     renderCountry(data);
 
-    // Get neighbor country
-    const [neighbor] = data.borders;
-    console.log(neighbor);
+//     // Get neighbor country
+//     const [neighbor] = data.borders;
+//     console.log(neighbor);
 
-    if (!neighbor) return;
+//     if (!neighbor) return;
 
-    // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbor}`);
-    request2.send();
+//     // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbor}`);
+//     request2.send();
 
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      console.log(data);
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data);
 
-      renderCountry(data2, 'neighbour');
-    });
-  });
-};
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
 
-getCountryAndNeighbor('Thailand');
+// getCountryAndNeighbor('Thailand');
 
-// Callback Hell Example
-setTimeout(() => {
-  console.log('1 second passed');
-  setTimeout(() => {
-    console.log('2 seconds passed');
-    setTimeout(() => {
-      console.log('3 seconds passed');
-      setTimeout(() => {
-        console.log('4 seconds passed');
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+// // Callback Hell Example
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 seconds passed');
+//       setTimeout(() => {
+//         console.log('4 seconds passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+///////////////////////////////////////
+// LESSON: Promises and the Fetch API
+///////////////////////////////////////
+
+// This is how we used to do it
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+// request.send();
+
+const requestFetch = fetch('https://restcountries.eu/rest/v2/name/thailand');
+console.log(requestFetch);
+
+// What are PROMISES?
+// -    A promise is an object that is used as a placeholder for the future
+//      result of an asynchronous operation.
+// -    A promise is a container for an asynchronous delivered value.
+// -    A promise is a container for a future value.
+//      For example, the response from an AJAX call
+
+//  What are the ADVANTAGES of using PROMISES?
+// 1)   We no longer need to rely on events and callbacks passed into
+//      asynchronous functions to handle asynchronous results
+// 2)   Instead of nesting callbacks, we can chain promises for
+//      a sequence of asynchronous operations: ESCAPING CALLBACK HELL!
+
+// The Promise LIFE-CYCLE
+// 1)   PENDING - before the future value is available
+// 2)   SETTLED - there are 2 types
+//      a) FULFILLED - success, the value is now available
+//      b) REJECTED - an error happened during the asynchronous task
+// *    we are able to handle these different states in our code
+// *    The outcome of a promise is settled only once and lasts forever
+// i.   Promises are 1st BUILT - such as the Fetch API returning a promise
+// ii.  Promises are 2nd CONSUMED - when we already have a promise
